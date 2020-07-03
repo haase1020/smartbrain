48e59fd81faaa275e2546809fe6b65e0ea4f4c23
Jon Haase <haase1020@gmail.com>
2020-07-03 05:44:43 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navigation from './components/Navigation/Navigation';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <Navigation />
+      <header className='App-header'>Hello World</header>
+
+      {/* <Logo />
+      <ImageLinkForm />
+      <FaceRecognition /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Navigation = () => {
+  return (
+    <nav>
+      <p>sign out</p>
+    </nav>
+  );
+};
+export default Navigation;
